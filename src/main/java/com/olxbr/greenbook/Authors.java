be300f847e1d3beb62eb6d268c41916796b5c884
deborapires <debora.pires@grupozap.com>
2021-06-02 16:47:05 -0300

    docker-compose

---
+++ src/main/java/com/olxbr/greenbook/Authors.java
@@ -0,0 +1,8 @@
+package com.olxbr.greenbook;
+
+public class Authors {
+    String first_name;
+    String last_name;
+    int code;
+    String nationality;
+}
--- src/main/java/com/olxbr/greenbook/GreenBookApplication.java
+++ src/main/java/com/olxbr/greenbook/GreenBookApplication.java
@@ -7,7 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreenBookApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GreenBookApplication.class, args);
+
+		Authors authors;
+		authors = new Authors();
+
+		authors.first_name = "William";
+		authors.last_name = "Shakespeare";
+		authors.code = 123;
+		authors.nationality = "English";
+
+		authors.first_name = "Guido";
+		authors.last_name = "van Rossum";
+		authors.code = 124;
+		authors.nationality = "Dutch";
 	}
 
 }
